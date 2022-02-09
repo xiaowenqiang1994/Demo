dde3a83bf90afddb8fa0542d2acbb9053ab4cfab
xiaowenqiang1994 <1061352189@qq.com>
2022-02-09 14:37:36 +0800

    增加了分页

--- post/views.py
+++ post/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
+from math import ceil
 
 # Create your views here.
 from .models import Post
+
+
+def post_list(request):
+    total = Post.objects.all().count()
+    pages = ceil(total / 5)
+    page = int(request.GET.get('page', 1))
+    start = (page - 1) * 5
+    end = start + 5
+
+    posts = Post.objects.all()[start:end]
+    return render(request, 'post_list.html',
+                  {'posts': posts, 'pages': range(1, pages + 1)})
+
 
 def post_create(request):
     if request.method == "POST":
@@ -32,7 +46,4 @@ def post_read(request):
     return render(request, 'read_post.html', {'post': post})
 
 
-def post_list(request):
-
-    return render(request, 'list_post.html', {})
 
